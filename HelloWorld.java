6b4c782c58f8f43d87ca9eb12368e93703959058
laihan323 <46051155+laihan323@users.noreply.github.com>
2020-03-07 15:35:05 +0800

    change zhangsan to wangmazi
    I update the output string.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
    public static void main(String args[]){
-	System.out.println("Hello,уехЩ");
+	System.out.println("Hello,wangmazi");
 	System.out.println("Hello,lisi");
    }
 }
